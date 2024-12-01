6b82aa6a04269affc8dc2f8b204ad94d50950a05
kairat <k_kurmanov@list.ru>
2024-12-01 16:23:38 +0600

    test

--- WebDev/settings/settings.py
+++ WebDev/settings/settings.py
@@ -144,8 +144,10 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'https://wheel-fortune-git-main-kairat-genius-projects.vercel.app',
 
 ]
